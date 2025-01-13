715a75abf1eb1dfb0a58d2bc6735ed14c9069cdb
Nathan Rodriguez <nathan94_roro@hotmail.fr>
2025-01-14 00:31:58 +0100

    Composant card

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types";
+import "./card.css";
+
+function Card({ image, alt, title, description }) {
+  return (
+    <div className="features">
+      <div className="feature-item">
+        <img src={image} alt={alt} className="feature-icon" />
+        <h3 className="feature-item-title">{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+Card.propTypes = {
+  image: PropTypes.string,
+  alt: PropTypes.string,
+  title: PropTypes.string,
+  description: PropTypes.string,
+};
+export default Card;
