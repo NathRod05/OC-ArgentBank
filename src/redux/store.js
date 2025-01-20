b88864b7ec2488203148838101fe3659b1aa446e
Nathan Rodriguez <nathan94_roro@hotmail.fr>
2025-01-21 00:37:26 +0100

    Ajout du store

---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import loginSlice from "./loginSlice";
+
+const store = configureStore({
+  reducer: {
+    login: loginSlice,
+  },
+  devTools: true,
+});
+
+export default store;
