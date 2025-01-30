19248045908fd7d91a07de8ce531ee59fa430123
Nathan Rodriguez <nathan94_roro@hotmail.fr>
2025-01-30 23:48:22 +0100

    Ajout de Useselector pour recuper les donnés du user logedIn

--- src/pages/user/User.jsx
+++ src/pages/user/User.jsx
@@ -1,18 +1,34 @@
 import Balance from "../../components/balance-card/BalanceCard";
 import UserName from "../../components/username/UserName";
 import "./user.css";
-
+import { useSelector } from "react-redux";
+import { userInfo } from "../../redux/userSlice";
+import { useState } from "react";
 import mockData from "../../../mock-data.json";
 import ModalEdit from "../../components/modal/ModalEdit";
 
 function User() {
+  const user = useSelector(userInfo);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <main className="container-user">
-      <UserName username="Tony" />
-      <div className="modal">
-        <div className="overlay"></div>
-        <ModalEdit />
-      </div>
+      <UserName username={user.userName} onOpen={handleOpenModal} />
+      {isModalOpen && (
+        <div className="overlay">
+          <div className="modal">
+            <ModalEdit onClose={handleCloseModal} />
+          </div>
+        </div>
+      )}
       {mockData.map((balance) => {
         return (
           <Balance
