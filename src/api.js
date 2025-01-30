6359ca4d27b23ea6dd826336f412333d70691ce1
Nathan Rodriguez <nathan94_roro@hotmail.fr>
2025-01-30 23:44:02 +0100

    Ajout Api post et get user

--- src/api.js
+++ src/api.js
@@ -5,11 +5,31 @@ async function loginUser(body) {
     body: JSON.stringify(body), // conversion au format JSON
   });
   if (response.ok) {
-    const loginInfo = await response.json();
-    return loginInfo;
+    const loginData = await response.json();
+    const loginToken = loginData.body.token;
+    return loginToken;
   } else {
     alert("Identifiants invalides. Veuillez réessayer.");
   }
 }
 
-export default loginUser;
+async function fetchUserProfile(token) {
+  const response = await fetch("http://localhost:3001/api/v1/user/profile", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  if (response.ok) {
+    const userDate = await response.json();
+    const userProfile = userDate.body;
+    return userProfile;
+  } else {
+    console.error("utilisateur introuvable");
+    return null;
+  }
+}
+
+export { loginUser, fetchUserProfile };
