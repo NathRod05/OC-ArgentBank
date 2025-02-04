d846275de7e9759602db8528c3ce2ba84320a2e2
Nathan Rodriguez <nathan94_roro@hotmail.fr>
2025-02-04 01:08:52 +0100

    Thunk et persist

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
-import store from "./redux/store.js";
+import { store, persistedStore } from "./redux/store.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistedStore}>
+        <App />
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
