8a38f45bcd047d7e39c0a4c0c3396d8ac1afc4fd
Nathan Rodriguez <nathan94_roro@hotmail.fr>
2025-02-04 01:07:59 +0100

    Ajout Thunk et persist pour eviter la deco au rechargement

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,34 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore, PERSIST } from "redux-persist";
+import { thunk } from "redux-thunk";
+import storage from "redux-persist/lib/storage";
 import loginReducer from "./loginSlice";
 import userReducer from "./userSlice";
 
-const store = configureStore({
+const userPersistConfig = {
+  key: "user",
+  storage,
+};
+const loginPersistConfig = {
+  key: "login",
+  storage,
+};
+
+const persistedUserReducer = persistReducer(userPersistConfig, userReducer);
+const persistedLoginReducer = persistReducer(loginPersistConfig, loginReducer);
+
+export const store = configureStore({
   reducer: {
-    login: loginReducer,
-    user: userReducer,
+    login: persistedLoginReducer,
+    user: persistedUserReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [PERSIST],
+      },
+    }).concat(thunk),
   devTools: true,
 });
 
-export default store;
+export const persistedStore = persistStore(store);
