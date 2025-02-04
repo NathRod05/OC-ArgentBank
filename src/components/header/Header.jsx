38855b5a32586ac535bebf61d3d753757cca8b3d
Nathan Rodriguez <nathan94_roro@hotmail.fr>
2025-02-04 01:08:38 +0100

    Ajout du Login logout

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,27 +1,50 @@
 import logo from "../../assets/argentBankLogo.png";
+import { useSelector, useDispatch } from "react-redux";
+import { tokenUser, logoutReducer } from "../../redux/loginSlice";
+import { userInfo, logout } from "../../redux/userSlice";
+
 import "./header.css";
 
 function Header() {
+  const token = useSelector(tokenUser);
+  const user = useSelector(userInfo);
+
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch(logoutReducer());
+    dispatch(logout());
+  };
+
   return (
     <header className="container-header">
       <a href="/">
         <img src={logo} alt="Argent Bank logo" className="logo"></img>
       </a>
       <div>
-        <a className="header-nav-item" href="/login">
-          <i className="fa fa-user-circle"></i>
-          Sign In
-        </a>
-        <div>
-          <a className="main-nav-item" href="./user">
+        {!token ? (
+          <a className="header-nav-item" href="/login">
             <i className="fa fa-user-circle"></i>
-            Tony
+            Sign In
           </a>
-          <a className="main-nav-item" href="./">
-            <i className="fa fa-sign-out"></i>
-            Sign Out
-          </a>
-        </div>
+        ) : (
+          <>
+            {user && user.userName ? (
+              <a className="main-nav-item name" href="./user">
+                <i className="fa fa-user-circle user"></i>
+                {user.userName}
+              </a>
+            ) : (
+              <div>
+                <i className="fa fa-user-circle user"></i>Loading...
+              </div>
+            )}
+            <a className="main-nav-item" href="/login" onClick={handleLogout}>
+              <i className="fa fa-sign-out logout"></i>
+              Sign Out
+            </a>
+          </>
+        )}
       </div>
     </header>
   );
